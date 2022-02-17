425a5b90773f0110b27991ab08f1c9f325629464
Sergio Eduardo D Oliveira <sergio.oliveira@mpdft.mp.br>
2022-02-17 12:44:30 -0300

    Repositorio/SUSI/nudes-4/nudes4#1 criação dinâmica de containers

--- build-and-recreate.sh
+++ build-and-recreate.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-WORKSPACE_MAPPING="./workspace"
+
+cd $(dirname "$0")
+
+PROJECT_NAME_KEY='PROJECT_NAME'
+PROJECT_NAME=$(cat .env | grep -e "$PROJECT_NAME_KEY" | sed "s/$PROJECT_NAME_KEY=//")
+
+WORKSPACE_MAPPING="./workspace-$PROJECT_NAME"
 if [ -d "$WORKSPACE_MAPPING" ]
 then
     rm -rf $WORKSPACE_MAPPING
-    mkdir -p $WORKSPACE_MAPPING
 fi
-docker compose up --force-recreate --build
+echo "(Re) criando pasta $WORKSPACE_MAPPING ..."
+mkdir -p $WORKSPACE_MAPPING
+
+if [ "${OSTYPE//[0-9.]/}" == "darwin" ]
+then
+    echo "MacOS detected... "
+    docker compose up --force-recreate --build
+else
+    echo "Other OS detected... "
+    sudo docker-compose up --force-recreate --build
+fi
